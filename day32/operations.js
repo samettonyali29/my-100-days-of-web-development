e763db4f0cf15c9362784c678d624448ebac71e7
samettonyali29 <samet.tonyali@gumushane.edu.tr>
2022-12-07 01:03:51 +0300

    git operations and git rules

--- day32/operations.js
+++ day32/operations.js
@@ -6,3 +6,16 @@ console.log(10 * 4);
 console.log(10 / 4);
 console.log(10 % 4);
 console.log(10 % 3);
+
+console.log((10 + 3 - 5) * 10);
+
+let result = (10 + 3 - 5) * 10;
+result = 10 * 4;
+result = result + 1;
+result++;
+result--;
+
+result += 5;
+result -= 5;
+
+console.log(result)
