2f8c5a3398ba85b535783044e3721978a4bd50ce
samettonyali29 <samet.tonyali@gumushane.edu.tr>
2022-12-11 23:59:21 +0300

    searched the DOM for elements

---
+++ day33/app.js
@@ -0,0 +1,4 @@
+//document.body.children[1].children[0].href = 'https://www.google.com';
+
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://www.google.com';
