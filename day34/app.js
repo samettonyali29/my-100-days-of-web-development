f74f1b5557c642095faeac8caabd491b0067f7ba
samettonyali29 <samet.tonyali@gumushane.edu.tr>
2022-12-13 00:40:38 +0300

    inserted new HTML elements via JavaScript

--- day34/app.js
+++ day34/app.js
@@ -5,3 +5,18 @@ anchorElement.href = 'https://www.google.com';
 
 anchorElement = document.querySelector('p a');
 anchorElement.href = 'http://yazilim.gumushane.edu.tr';
+
+//ADD AN ELEMENT
+//1 - Create the new element
+
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href = 'https://www.gumushane.edu.tr';
+newAnchorElement.textContent = "This leads to Gümüşhane University!";
+
+//2 - Get access to the parent element that should hold the new element
+
+let firstParagraph = document.querySelector('p');
+
+//3 - Insert the new element into the parent element content
+
+firstParagraph.append(newAnchorElement);
