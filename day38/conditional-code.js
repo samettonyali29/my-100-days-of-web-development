1bc4ea48600b7ca8bf3329c4697e9f31be1b1d96
samettonyali29 <samet.tonyali@gumushane.edu.tr>
2022-12-28 23:39:47 +0300

    more examples on booleans

---
+++ day38/conditional-code.js
@@ -0,0 +1,17 @@
+const myName = 'Samet';
+
+if (myName === 'Samet') {
+    console.log('Hello!');
+}
+
+let isLoggedIn = true;
+
+if (isLoggedIn) {
+    console.log('User is logged in!');
+}
+
+const enteredUserName = ''; // treated like it is false
+
+if (enteredUserName) {
+    console.log('Input is valid!');
+}
