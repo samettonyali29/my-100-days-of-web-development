9a92104af4d1c3505caa18675fb403865ea97f38
samettonyali29 <samet.tonyali@gumushane.edu.tr>
2022-12-29 23:10:43 +0300

    practice time: for-in loop completed

--- day39/loops-in-action/loops-in-action.js
+++ day39/loops-in-action/loops-in-action.js
@@ -34,3 +34,28 @@ function highlightLinks () {
 }
 
 highlightLinksButtonElement.addEventListener('click', highlightLinks);
+
+// Display user data
+
+const dummyUserData = {
+    firstName: 'Samet',
+    lastName: 'Tonyali',
+    age: 34
+};
+
+const displayUserDataButtonElement = document.querySelector('#user-data button');
+
+function displayUserData () {
+    const outputDataElement = document.getElementById('output-user-data');
+
+    outputDataElement.innerHTML = '';
+
+    for (const key in dummyUserData) {
+        const newUserDataListItemElement = document.createElement('li');
+        const outputText = key.toUpperCase() + ': ' + dummyUserData[key];
+        newUserDataListItemElement.textContent = outputText;
+        outputDataElement.append(newUserDataListItemElement);
+    }
+}
+
+displayUserDataButtonElement.addEventListener('click', displayUserData);
