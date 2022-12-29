def37ed5aaef96e2b63e1638ebfb2801145b3a9d
samettonyali29 <samet.tonyali@gumushane.edu.tr>
2022-12-29 22:40:46 +0300

    while example added

--- day38/loops.js
+++ day38/loops.js
@@ -18,3 +18,11 @@ for (const propertyName in loggedInUser) {
     console.log(propertyName);
     console.log(loggedInUser[propertyName]);
 }
+
+let isFinished = false;
+
+while (!isFinished) {
+    isFinished = confirm('Do you want to quit?');
+}
+
+console.log('Done!');
