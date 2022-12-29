ed031df13e86ed2a00ed703aac91b9c35f871a9c
samettonyali29 <samet.tonyali@gumushane.edu.tr>
2022-12-29 22:30:56 +0300

    for-in example added

--- day38/loops.js
+++ day38/loops.js
@@ -7,3 +7,14 @@ const users = ['Samet', 'Fulya', 'Tomris'];
 for (const user of users) {
     console.log(user);
 }
+
+const loggedInUser = {
+    name: 'Samet',
+    age: 34,
+    isAdmin: true
+};
+
+for (const propertyName in loggedInUser) {
+    console.log(propertyName);
+    console.log(loggedInUser[propertyName]);
+}
