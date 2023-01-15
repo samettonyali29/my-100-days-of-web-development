64ffe9c0984c5a061dd4ba72c54258cf068e6daa
samettonyali29 <samet.tonyali@gumushane.edu.tr>
2023-01-15 22:37:38 +0300

    stored data in (server-side) files

--- day48/app.js
+++ day48/app.js
@@ -1,5 +1,8 @@
 // node day47/app.js
 
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
@@ -16,7 +19,16 @@ app.get('/', function (request, response) {
 
 app.post('/store-user', function (request, response) {
     const userName = request.body.username;
-    console.log(userName);
+    
+    const filePath = path.join(__dirname, 'data', 'users.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+
+    existingUsers.push(userName);
+
+    fs.writeFileSync(filePath, JSON.stringify(existingUsers));
+
     response.send('<h1>Username stored!</h1>');
 });
 
