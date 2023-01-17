62cf9b9d7471ae3fa0603d061c922560b150dbe2
samettonyali29 <samet.tonyali@gumushane.edu.tr>
2023-01-17 18:01:27 +0300

    added the EJS template engine

--- day50/app.js
+++ day50/app.js
@@ -5,32 +5,30 @@ const express = require('express');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', function (request, response) {
-    const htmlFilePath = path.join(__dirname, 'views', 'index.html');
-    response.sendFile(htmlFilePath);
+    response.render('index');
 });
 
 app.get('/restaurants', function (request, response) {
-    const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
-    response.sendFile(htmlFilePath);
+    response.render('restaurants');
 });
 
 app.get('/about', function (request, response) {
-    const htmlFilePath = path.join(__dirname, 'views', 'about.html');
-    response.sendFile(htmlFilePath);
+    response.render('about');
 });
 
 app.get('/confirm', function (request, response) {
-    const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
-    response.sendFile(htmlFilePath);
+    response.render('confirm');
 });
 
 app.get('/recommend', function (request, response) {
-    const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
-    response.sendFile(htmlFilePath);
+    response.render('recommend');
 });
 
 app.post('/recommend', function (request, response) {
