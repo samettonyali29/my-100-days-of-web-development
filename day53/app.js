fc73a183fec31d82a3aadd40982ad383bff6638f
samettonyali29 <samet.tonyali@gumushane.edu.tr>
2023-01-27 16:58:32 +0300

    used the Express Router to split the Route configuration

--- day53/app.js
+++ day53/app.js
@@ -1,10 +1,9 @@
-const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
-const uuid = require('uuid');
 
-const resData = require('./util/restaurant-data');
+const defaultRoutes = require('./routes/default');
+const restaurantRoutes = require('./routes/restaurants');
 
 const app = express();
 
@@ -14,52 +13,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', function (request, response) {
-    response.render('index');
-});
-
-app.get('/restaurants', function (request, response) {
-    const storedRestaurants = resData.getStoredRestaurants();
-
-    response.render('restaurants', { numberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants });
-});
-
-app.get('/restaurants/:id', function (request, response) {
-    const restaurantId = request.params.id;
-    const storedRestaurants = resData.getStoredRestaurants();
-
-    for (const restaurant of storedRestaurants) {
-        if (restaurant.id === restaurantId) {
-            return response.render('restaurant-detail', { restaurant: restaurant });
-        }
-    }
-
-    response.status(404).render('404');
-});
-
-app.get('/about', function (request, response) {
-    response.render('about');
-});
-
-app.get('/confirm', function (request, response) {
-    response.render('confirm');
-});
-
-app.get('/recommend', function (request, response) {
-    response.render('recommend');
-});
-
-app.post('/recommend', function (request, response) {
-    const restaurant = request.body;
-    restaurant.id = uuid.v4();
-    const restaurants = resData.getStoredRestaurants();
-
-    restaurants.push(restaurant);
-
-    storeRestaurants(restaurants);
-
-    response.redirect('/confirm');
-});
+app.use('/', defaultRoutes);
+app.use('/', restaurantRoutes);
 
 app.use(function (request, response) {
     response.status(404).render('404');
