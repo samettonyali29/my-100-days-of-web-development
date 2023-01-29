57d932dc104dda7a687bee0698c63c8e3897cb0e
samettonyali29 <samet.tonyali@gumushane.edu.tr>
2023-01-29 19:53:25 +0300

    asynchronous code and error handling

--- day56/async.js
+++ day56/async.js
@@ -8,9 +8,17 @@ function readFile () {
 //        // start another async task that sends the data to a database
 //    });
 
-    fs.readFile('data.txt').then(function(fileData) {
+    fs.readFile('data.txt')
+    .then(function(fileData) {
         console.log('File parsing done!');
         console.log(fileData.toString());
+        // return anotherAsyncOperation;
+    })
+    .then(function() {
+
+    })
+    .catch(function(error) {
+        console.log(error);
     });
 
     console.log('Hi there!');
