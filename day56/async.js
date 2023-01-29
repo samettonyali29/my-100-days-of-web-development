1ceba596cddbe42d30aa0f927772db74bc3bb974
samettonyali29 <samet.tonyali@gumushane.edu.tr>
2023-01-29 19:50:37 +0300

    introduced promises

--- day56/async.js
+++ day56/async.js
@@ -1,8 +1,14 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 
 function readFile () {
     let fileData;
-    fileData = fs.readFile('data.txt', function(error, fileData) {
+//    fileData = fs.readFile('data.txt', function(error, fileData) {
+//        console.log('File parsing done!');
+//        console.log(fileData.toString());
+//        // start another async task that sends the data to a database
+//    });
+
+    fs.readFile('data.txt').then(function(fileData) {
         console.log('File parsing done!');
         console.log(fileData.toString());
     });
